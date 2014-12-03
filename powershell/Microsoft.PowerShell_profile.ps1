07383cfe31fa40b1f2bbf8b58282d5fd3c58d70d
Tim Goshinski <tim@code-chimp.com>
2014-12-03 12:15:33 -0800

    clean up powershell config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,19 @@
 $pshost = get-host
 $pswindow = $pshost.ui.rawui
+$x86Path = (get-item 'Env:ProgramFiles(x86)').Value
 
-$pswindow.windowtitle="PS A-Go-Go"
-$pswindow.backgroundcolor = "Black"
-$pswindow.foregroundcolor = "Gray"
+$pswindow.windowtitle='PS A-Go-Go'
+$pswindow.backgroundcolor = 'Black'
+$pswindow.foregroundcolor = 'Gray'
 
-$x86Path = (get-item "Env:ProgramFiles(x86)").Value
+$env:path += ';' + (get-item 'Env:ProgramFiles(x86)').value + '\Git\bin'
 
-$env:path += ";" + (get-item "Env:ProgramFiles(x86)").Value + "\Git\bin"
-
-set-alias fsi ($x86Path + "\Microsoft SDKs\F#\4.0\Framework\v4.0\fsi.exe")
-
-if(!(Test-Path Function:\TabExpansion)) { New-Item Function:\Global:TabExpansion -value '' | Out-Null }
+set-alias fsi ($x86Path + '\Microsoft SDKs\F#\4.0\Framework\v4.0\fsi.exe')
 
 # load virtualenv wrapper
 #import-module virtualenvwrapper
 
 # Load posh-git example profile
-. ($HOME + "\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1")
+. ($HOME + '\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1')
 
 #clear-host
